d6eeadcd8b73de4294ca9637629645a004149813
lucasarruda12 <lucas123manoel4@gmail.com>
2025-10-16 16:16:23 -0300

    fixed tabs

--- src/ff_d.vhd
+++ src/ff_d.vhd
@@ -2,19 +2,19 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity ff_d is
-	port (
-		d 	: in 	BIT;
-		clk 	: in 	BIT;
-		q	: out	BIT
-	);
+  port (
+    d   : in  BIT;
+    clk : in  BIT;
+    q   : out BIT
+  );
 end ff_d;
 
 architecture arch of ff_d is
 begin
-	process(clk)
-	begin
-		if clk='1' then
-			q <= d;
-		end if;
-	end process;
+  process(clk)
+  begin
+    if clk='1' then
+      q <= d;
+    end if;
+  end process;
 end arch;
